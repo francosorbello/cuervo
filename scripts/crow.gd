d6ad4bb8e27a5b7ea7c739165c3e501f0f3db636
francosorbello <franco.sorbelo@gmail.com>
2024-02-05 01:18:49 -0300

    crows look at and follow the player

--- scripts/crow.gd
+++ scripts/crow.gd
@@ -1,8 +1,17 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-
+@export var speed: float = 50
 @export var health: Node2D
+
+var player: Node
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+
+func _physics_process(delta):
+	velocity = global_position.direction_to(player.global_position)
+	move_and_collide(velocity*speed*delta)
+	look_at(player.global_position)
 
 func take_damage(damage: float):
 	health.take_damage(damage)
