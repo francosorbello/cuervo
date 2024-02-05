d913237fc16ba1ef58e2910b5270a41b266f1b64
francosorbello <franco.sorbelo@gmail.com>
2024-02-04 21:40:36 -0300

    first commit

---
+++ scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export var speed = 400.0 
+@export var movement_deceleration: float = 0.8
+@export var aim_deceleration: float = 0.8
+
+var previous_motion: Vector2 = Vector2.ZERO
+var previous_aim_position: Vector2 = get_global_mouse_position()
+
+func get_input():
+	var input_dir = Input.get_vector("left", "right", "up", "down").normalized()
+	velocity = input_dir * speed
+
+func _physics_process(delta):
+	get_input()
+
+	_handle_movement(delta)
+	_handle_aim()	
+
+func _handle_movement(delta: float):
+	var current_motion: Vector2 = velocity * delta
+	current_motion = current_motion.lerp(previous_motion, movement_deceleration)
+	
+	move_and_collide(current_motion)
+	
+	previous_motion = current_motion
+
+func _handle_aim():
+	var current_aim_position: Vector2 = get_global_mouse_position()
+	current_aim_position = current_aim_position.lerp(previous_aim_position,aim_deceleration) 
+	
+	look_at(current_aim_position)
+	
+	previous_aim_position = current_aim_position
+	
