d5c9ba8091b1db818496491d87f69f3df77c4f80
francosorbello <franco.sorbelo@gmail.com>
2024-02-10 15:09:03 -0300

    added music to game

--- scripts/crow.gd
+++ scripts/crow.gd
@@ -5,15 +5,18 @@ extends CharacterBody2D
 @export var attack_damage: float = 20
 
 var player: Node
+var allow_movement: bool = true
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	$Animations.play("move")
 
 func _physics_process(delta):
+	look_at(player.global_position)
+	if(!allow_movement):
+		return
 	velocity = global_position.direction_to(player.global_position)
 	move_and_collide(velocity*speed*delta)
-	look_at(player.global_position)
 
 func take_damage(damage: float):
 	health.take_damage(damage)
@@ -28,3 +31,18 @@ func _on_attack_detect_zone_body_entered(_body):
 func _on_hitbox_body_entered(body:Node2D):
 	if(body.is_in_group("player")):
 		body.take_damage(attack_damage)
+
+func toggle_walk():
+	$Animations.play("move")
+	enable_movement()
+
+func enable_movement():
+	allow_movement = true
+
+func disable_movement():
+	allow_movement = false
+
+
+func _on_attack_detect_zone_body_exited(body:Node2D):
+	if(body.is_in_group("player")):
+		toggle_walk()
