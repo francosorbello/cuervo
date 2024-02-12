a92f2f9d24e4313a676faa06d811ded0b5519703
francosorbello <franco.sorbelo@gmail.com>
2024-02-11 23:48:16 -0300

    added crow move state

---
+++ scripts/crow/states/MoveState.gd
@@ -0,0 +1,11 @@
+extends State
+
+func enter():
+    print("start idle state")
+    state_owner.get_animation_player().move()
+    pass
+
+func pyhsics_update(delta):
+    state_owner.look_at(state_owner.get_player().global_position)
+    state_owner.velocity = state_owner.global_position.direction_to(state_owner.get_player().global_position)
+    state_owner.move_and_collide(state_owner.velocity*state_owner.speed*delta)
