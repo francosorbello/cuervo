b361f13e7d9d5e96936768c3e19498a2c3e89b59
francosorbello <franco.sorbelo@gmail.com>
2024-02-11 23:22:08 -0300

    added setup, enter, update and exit funcs to states. added state machine

--- scripts/state.gd
+++ scripts/state.gd
@@ -3,6 +3,18 @@ class_name State extends Node
 var state_machine: StateMachine = null
 var state_owner: Node = null
 
+func setup(s_owner: Node, s_machine: StateMachine):
+    self.state_machine = s_owner
+    self.state_owner = s_machine
+
 func enter():
     pass
 
+func update(delta: float):
+    pass
+
+func pyhsics_update(delta: float):
+    pass
+
+func exit():
+    pass
