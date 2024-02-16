08c33e31a88acc6373a8ac1af30568209e911904
francosorbello <franco.sorbelo@gmail.com>
2024-02-15 23:44:50 -0300

    story manager launches dialogue ui on trigger

--- scripts/StoryManager.gd
+++ scripts/StoryManager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var start_timer : float = 0.5
+@export var initial_dialogue : Dialogue
 
 var started : bool = false
 
@@ -14,6 +15,8 @@ func _on_trigger_area_body_entered(body:Node2D):
         await get_tree().create_timer(start_timer).timeout
         $Player.story_zoom_in()
         $Player.story_disable_controller()
+        $UI/DialogueUI.start_dialogue(initial_dialogue)
+        
 
 func test_zoom_out():
     await get_tree().create_timer(3).timeout
--- scripts/ui/dialogue_ui.gd
+++ scripts/ui/dialogue_ui.gd
@@ -2,21 +2,30 @@ extends Control
 
 @export var line_label : RichTextLabel
 @export var line_velocity : float = 0.5
-
 @export_range(1,3) var max_rows : int = 3 
 
+@export_category("Auto start")
 @export var autostart : bool = false
 @export var initial_dialogue : Dialogue
+
+@export_category("Fade")
+@export var fade_in_duration : float = 0.5
+@export var fade_out_duration : float = 0.5
+@export var hide_on_start : bool = true
 
 var current_dialogue : Dialogue
 var current_line : int = 0
 var current_row : int = 0
 
 func _ready():
+    if(hide_on_start):
+        modulate = Color(0,0,0,1)
+        
     if(autostart):
         start_dialogue(initial_dialogue)
 
 func start_dialogue(dialogue: Dialogue):
+    await fade_in()
     if(dialogue == null):
         return
 
@@ -30,23 +39,23 @@ func read_line(line : DialogueLine):
     if(line_label != null):
 
         if(current_row == max_rows or line.single_row):
-            # line_label.text = ""
             line_label.clear()
-            print("rows surpassed")
             current_row = 0
         else:
             current_row += 1
         
         line_label.visible_characters = line_label.get_parsed_text().length()
+       
         var previous_line = line_label.get_parsed_text()
         line_label.append_text(center_text(line.Line))
+       
         line_label.visible_ratio = get_visible_ratio(previous_line,line.Line)
         tween.tween_property(line_label,"visible_ratio",1,line_velocity)
+       
         $LineTimer.start(line.duration)
     pass
 
 func get_visible_ratio(prev_line, new_line) -> float:
-    print("prev line length: %s"%prev_line.length())
     var total_characters = prev_line.length() + new_line.length()
     return float(prev_line.length()) / total_characters
 
@@ -56,11 +65,22 @@ func center_text(text : String) -> String:
 func next_line():
     if(current_dialogue.lines[current_line].single_row):
         line_label.clear()
-        
+
     current_line += 1
     if current_line < len(current_dialogue.lines):
         read_line(current_dialogue.lines[current_line])
-    pass
+    else:
+        fade_out()
 
 func _on_line_timer_timeout():
     next_line()
+
+func fade_in():
+    await _do_fade(1,fade_in_duration)
+
+func fade_out():
+    await _do_fade(0,fade_out_duration,0)
+
+func _do_fade(value: float,time: float, override_transparency: float = 1):
+    var tween = get_tree().create_tween()
+    await tween.tween_property(self,"modulate",Color(value,value,value,override_transparency),time).finished
