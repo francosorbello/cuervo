a7ab29a4389a319d6c2da635262b4f2740fc7afb
francosorbello <franco.sorbelo@gmail.com>
2024-02-15 23:23:47 -0300

    added support for line rows in dialogue ui

--- scripts/dialogues/DialogueLine.gd
+++ scripts/dialogues/DialogueLine.gd
@@ -3,4 +3,5 @@ extends Resource
 class_name DialogueLine
 
 @export var Line : String
-@export var duration : float = 1
+@export var duration : float = 5
+@export var single_row : bool = false
--- scripts/ui/dialogue_ui.gd
+++ scripts/ui/dialogue_ui.gd
@@ -2,11 +2,15 @@ extends Control
 
 @export var line_label : RichTextLabel
 @export var line_velocity : float = 0.5
-@export var initial_dialogue : Dialogue
+
+@export_range(1,3) var max_rows : int = 3 
+
 @export var autostart : bool = false
+@export var initial_dialogue : Dialogue
 
 var current_dialogue : Dialogue
 var current_line : int = 0
+var current_row : int = 0
 
 func _ready():
     if(autostart):
@@ -18,22 +22,41 @@ func start_dialogue(dialogue: Dialogue):
 
     current_dialogue = dialogue
     current_line = 0
-    read_line(dialogue.lines[0])
+    read_line(current_dialogue.lines[0])
 
 func read_line(line : DialogueLine):
     var tween = get_tree().create_tween()
+    
     if(line_label != null):
-        line_label.visible_characters = line_label.text.length()
-        line_label.text = center_text(line.Line)
-        line_label.visible_ratio = 0
+
+        if(current_row == max_rows or line.single_row):
+            # line_label.text = ""
+            line_label.clear()
+            print("rows surpassed")
+            current_row = 0
+        else:
+            current_row += 1
+        
+        line_label.visible_characters = line_label.get_parsed_text().length()
+        var previous_line = line_label.get_parsed_text()
+        line_label.append_text(center_text(line.Line))
+        line_label.visible_ratio = get_visible_ratio(previous_line,line.Line)
         tween.tween_property(line_label,"visible_ratio",1,line_velocity)
         $LineTimer.start(line.duration)
     pass
+
+func get_visible_ratio(prev_line, new_line) -> float:
+    print("prev line length: %s"%prev_line.length())
+    var total_characters = prev_line.length() + new_line.length()
+    return float(prev_line.length()) / total_characters
 
 func center_text(text : String) -> String:
     return "[center] %s [/center]" % text
 
 func next_line():
+    if(current_dialogue.lines[current_line].single_row):
+        line_label.clear()
+        
     current_line += 1
     if current_line < len(current_dialogue.lines):
         read_line(current_dialogue.lines[current_line])
